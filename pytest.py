1cadf10e0848f7a94877b108372e073b78d40aeb
Mert POLAT <90008958+mert2m@users.noreply.github.com>
2024-02-21 15:18:13 +0300

    Update pytest.py

--- pytest.py
+++ pytest.py
@@ -1,41 +1,18 @@
 import pytest
 import json
 from app import app, db, Task
+from tests.conftest import client
+
+
+
 
 @pytest.fixture
 def client():
-    app.config['TESTING'] = True
-    client = app.test_client()
-
-    with app.app_context():
-        db.create_all()
+    app = create_app({"TESTING": True})
+    with app.test_client() as client:
         yield client
-        db.session.remove()
-        db.drop_all()
 
-def test_get_all_tasks_empty(client):
-    response = client.get('/get')
-    assert response.status_code == 200
-    assert json.loads(response.data) == {'tasks': []}
 
-def test_create_task(client):
-    task_data = {'content': 'Task 1', 'completed': 'no'}
-    response = client.post('/post', json=task_data)
-    assert response.status_code == 200
-    assert json.loads(response.data) == {'message': 'Task created successfully'}
-
-    response = client.get('/get')
-    assert response.status_code == 200
-    assert json.loads(response.data) == {'tasks': [{'content': 'Task 1', 'completed': 'no'}]}
-
-def test_delete_task(client):
-    task_data = {'content': 'Task 1', 'completed': 'no'}
-    client.post('/post', json=task_data)
-
-    response = client.delete('/home/delete/Task 1')
-    assert response.status_code == 200
-    assert json.loads(response.data) == {'message': 'Task deleted successfully'}
-
-    response = client.get('/get')
-    assert response.status_code == 200
-    assert json.loads(response.data) == {'tasks': []}
+def test_should_status_code_ok(client):
+	response = client.get('/get')
+	assert response.status_code == 200
